627777498c65174a6ae05f5743c44aeeea3d15ef
Dfeng4150 <70169968+Dfeng4150@users.noreply.github.com>
2020-08-25 18:28:51 -0700

    Homework 1

---
+++ Feng_Denny_HW1_main.c
@@ -0,0 +1,20 @@
+/**************************************************************
+* Class:  CSC-415-0# Fall 2020
+* Name:Denny Feng
+* Student ID:918748225
+* Github Name:Dfeng4150
+* Project: C-Workshop, Assignment 1 – Command Line Arguments
+*
+* File: Feng_Denny_HW1_main.c
+*
+* Description:To get started with using c commands and Makefile.
+*
+**************************************************************/
+#include <stdio.h>
+
+int main (int argc, char * argv[])
+{
+	printf("Hello CSC415, I'm Denny Feng!\n");
+
+
+}
