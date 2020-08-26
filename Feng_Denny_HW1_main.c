99c280e73727324062ee2b14f6f74381e9989e63
Dfeng4150 <70169968+Dfeng4150@users.noreply.github.com>
2020-08-25 20:14:11 -0700

    Homework 1

--- Feng_Denny_HW1_main.c
+++ Feng_Denny_HW1_main.c
@@ -3,7 +3,7 @@
 * Name:Denny Feng
 * Student ID:918748225
 * Github Name:Dfeng4150
-* Project: C-Workshop, Assignment 1 – Command Line Arguments
+* Project: Command Line, Assignment 1 – Command Line Arguments
 *
 * File: Feng_Denny_HW1_main.c
 *
@@ -14,7 +14,15 @@
 
 int main (int argc, char * argv[])
 {
-	printf("Hello CSC415, I'm Denny Feng!\n");
+	int i;
 
+        printf( "There were %d arguments on the command line.\n", argc); 
+        printf( "Argument 00: %s\n", argv[0] );
 
+    	for ( i = 1; i < argc; ++i ) {
+                  printf( "Argument 0%d: %s\n", i, argv[i] );
+                }
+        
+
+        return 0;
 }
